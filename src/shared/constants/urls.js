9fc342e5c1564aae33a0230cf9e7d41551d33ba6
Austin Spinazze <spinazzeaustin@yahoo.com>
2021-05-27 15:00:04 -0500

    - Bumped dns-packet to 1.3.4 - Updated LinkedIn url - Added link preview with image

--- src/shared/constants/urls.js
+++ src/shared/constants/urls.js
@@ -1,3 +1,3 @@
 export const GITHUB = "https://github.com/AustinSpinazze";
 export const INSTAGRAM = "https://www.instagram.com/austinspinazzedev/";
-export const LINKEDIN = "https://www.linkedin.com/in/austin-spinazze-576853121/";
+export const LINKEDIN = "https://www.linkedin.com/in/austin-spinazze/";
